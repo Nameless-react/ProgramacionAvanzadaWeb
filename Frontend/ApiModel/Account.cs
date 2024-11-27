dddd0297c2248566b6d850c4fef8c2b697256f0b
AdriiViquez <101229435+AdriiViquez@users.noreply.github.com>
2024-11-26 19:33:05 -0600

    Frontend Account Creado
    Frontend Account Creado

---
+++ Frontend/ApiModel/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace Frontend.ApiModel
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        public string AccountNumber { get; set; } = null!;
+
+        public int ClientId { get; set; }
+
+        public int AccountTypeId { get; set; }
+
+        public decimal? Balance { get; set; }
+
+        public DateTime? OpeningDate { get; set; }
+    }
+}
---
+++ Frontend/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Frontend.Helpers.Interface;
+using Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAccountHelper _accountHelper;
+
+        public AccountController(IAccountHelper accountHelper) 
+        { 
+            _accountHelper = accountHelper;
+        }
+
+        public ActionResult Index() 
+        {
+            var list = _accountHelper.GetAccounts();
+            return View(list);
+        }
+
+        public ActionResult Details(int id) 
+        {
+            var account = _accountHelper.GetAccount(id);
+            return View(account);
+        }
+
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AccountViewModel account) 
+        {
+            try
+            {
+                _accountHelper.Add(account);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e) 
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id) 
+        {
+            var account = _accountHelper.GetAccount(id);
+            return View(account);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AccountViewModel account) 
+        {
+            try
+            {
+                _accountHelper.Update(account);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e) 
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            var account = _accountHelper.GetAccount(id);
+
+            return View(account);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(AccountViewModel account)
+        {
+            try
+            {
+                _accountHelper.Delete(account.AccountId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
