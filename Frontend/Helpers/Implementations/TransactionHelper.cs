1f0c6cc63aab53148431db3794bfc7153e6247dd
AdriiViquez <101229435+AdriiViquez@users.noreply.github.com>
2024-12-04 12:47:44 -0600

    Frontend de Transacciones
    Frontend de Transacciones

---
+++ Frontend/Helpers/Implementations/TransactionHelper.cs
@@ -0,0 +1,110 @@
+﻿using Frontend.Helpers.Interface;
+using FrontEnd.Helpers.Interfaces;
+using Frontend.Models;
+using Newtonsoft.Json;
+using Frontend.ApiModel;
+
+namespace Frontend.Helpers.Implementations
+{
+    public class TransactionHelper : ITransactionHelper
+    {
+        IServiceRepository _ServiceRepository;
+        public TransactionHelper(IServiceRepository serviceRepository)
+        {
+            _ServiceRepository = serviceRepository;
+        }
+        Transaction Convertir(TransactionViewModel transaction)
+        {
+            return new Transaction
+            {
+                TransactionId = transaction.TransactionId,
+                OriginAccountId = transaction.OriginAccountId,
+                DestinationAccountId = transaction.DestinationAccountId,
+                TransactionDate = transaction.TransactionDate,
+                Amount = transaction.Amount,
+                Description = transaction.Description
+            };
+        }
+
+        public TransactionViewModel Add(TransactionViewModel transaction)
+        {
+            HttpResponseMessage response = _ServiceRepository.PostResponse("api/Transaction", Convertir(transaction));
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+            }
+            return transaction;
+        }
+
+        public void Delete(int id)
+        {
+            HttpResponseMessage responseMessage = _ServiceRepository.DeleteResponse("api/Transaction/" + id.ToString());
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var content = responseMessage.Content;
+            }
+        }
+
+        public TransactionViewModel Get(int id)
+        {
+            HttpResponseMessage responseMessage = _ServiceRepository.GetResponse("api/Transaction/" + id.ToString());
+            Transaction transaction = new Transaction();
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                transaction = JsonConvert.DeserializeObject<Transaction>(content);
+            }
+            TransactionViewModel resultado = new TransactionViewModel
+            {
+                TransactionId = transaction.TransactionId,
+                OriginAccountId = transaction.OriginAccountId,
+                DestinationAccountId = transaction.DestinationAccountId,
+                TransactionDate = transaction.TransactionDate,
+                Amount = transaction.Amount,
+                Description = transaction.Description
+            };
+            return resultado;
+        }
+
+        public List<TransactionViewModel> GetTransactions()
+        {
+            HttpResponseMessage responseMessage = _ServiceRepository.GetResponse("api/Transaction");
+            List<Transaction> transactions = new List<Transaction>();
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                transactions = JsonConvert.DeserializeObject<List<Transaction>>(content);
+            }
+            List<TransactionViewModel> result = new List<TransactionViewModel>();
+            foreach (var item in transactions)
+            {
+                result.Add(
+                    new TransactionViewModel
+                    {
+                        TransactionId = item.TransactionId,
+                        OriginAccountId = item.OriginAccountId,
+                        DestinationAccountId = item.DestinationAccountId,
+                        TransactionDate = item.TransactionDate,
+                        Amount = item.Amount,
+                        Description = item.Description
+                    }
+                    );
+            }
+            return result;
+        }
+
+
+
+        public TransactionViewModel Update(TransactionViewModel transaction)
+        {
+            HttpResponseMessage response = _ServiceRepository.PutResponse("api/Transaction/" + transaction.TransactionId.ToString(), Convertir(transaction));
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+
+            }
+            return transaction;
+        }
+    }
+}
+
---
+++ Frontend/Helpers/Interface/ITransactionHelper.cs
@@ -0,0 +1,13 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Helpers.Interface
+{
+    public interface ITransactionHelper
+    {
+        List<TransactionViewModel> GetTransactions();
+        TransactionViewModel Get(int id);
+        TransactionViewModel Add(TransactionViewModel transaction);
+        TransactionViewModel Update(TransactionViewModel transaction);
+        void Delete(int id);
+    }
+}
