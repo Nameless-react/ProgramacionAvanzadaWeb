89022fbff15927ff8f4234d232594a1b78f88f4b
joel <joel33960@gmail.com>
2024-12-15 14:57:49 -0600

    cambios en los dto

--- Frontend/Models/UserViewModel.cs
+++ Frontend/Models/UserViewModel.cs
@@ -10,6 +10,7 @@ namespace Frontend.Models
 
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
         public bool RememberLogin { get; set; }
         public string? ReturnUrl { get; set; }
 
